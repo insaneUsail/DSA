b7dca29abfaf8444b48e23d3f8f7c19fa0c7f722
Usail khan <86511043+insaneUsail@users.noreply.github.com>
2025-08-02 00:06:23 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (98.74%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,12 +3,15 @@ public:
     vector<vector<int>> generate(int numRows) {
 
          vector<vector<int>> pascal;
-        for(int i = 0; i<numRows; i++){
-            vector<int> row(i+1, 1);
-            for(int j = 1; j< i; j++){
-                row[j] = pascal[i-1][j-1] + pascal[i-1][j];
-            }
-            pascal.push_back(row);
+        for(int i=0; i< numRows;i++){
+         pascal.push_back(vector<int>(i + 1, 1));
+        for(int j=1;j<i;j++){
+            
+pascal[i][j]=pascal[i-1][j-1]+pascal[i-1][j];
+
+
+        }
+        
         }
         return pascal;
     }
