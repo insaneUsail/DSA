a8b1015b4c981120d9f3abf48ae3a95c0cb100fa
Usail khan <86511043+insaneUsail@users.noreply.github.com>
2025-08-18 14:34:28 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (65.61%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -34,3 +34,11 @@ public:
         return in.empty() && out.empty();
     }
 };
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
