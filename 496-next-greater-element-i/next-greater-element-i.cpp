62cee569ebff7ff54cef580dafb5a8caf7326609
Usail khan <86511043+insaneUsail@users.noreply.github.com>
2025-08-26 22:47:08 +0530

    Time: 3 ms (42.00%) | Memory: 12.8 MB (38.23%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> ng;
+        unordered_map<int, int> hash;
         stack<int> st;
 
         for (int num : nums2) {
-            while (!st.empty() && st.top() < num) {
-                ng[st.top()] = num;
+            while(!st.empty() && num > st.top()) {
+                hash[st.top()] = num;
                 st.pop();
             }
             st.push(num);
         }
-
-        vector<int> res;
-        for (int num : nums1) {
-            res.push_back(ng.count(num) ? ng[num] : -1);
-        }
-        return res;        
+vector<int> vt;
+for(int num:nums1){
+    vt.push_back(hash.count(num)?hash[num]:-1);
+}
+return vt;
     }
 };
