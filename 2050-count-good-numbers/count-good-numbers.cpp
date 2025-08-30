70ad8dee4c6a6b4e10d04512c2ba7e384aa4664f
Usail khan <86511043+insaneUsail@users.noreply.github.com>
2025-08-31 01:39:51 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (36.40%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -1,24 +1,20 @@
 class Solution {
-private:
-    int m = 1e9 + 7;
-    int binpow(long long a, long long b) {
-        int res = 1;
-        while (b > 0) {
-            if (b & 1)
-                res = res * a % m;
-            a = a * a % m;
-            b >>= 1;
+public:
+    const int MOD = 1e9 + 7;
+
+    long long modPow(long long base, long long exp) {
+        if (exp == 0) return 1; // base case
+        long long half = modPow(base, exp / 2);
+        long long res = (half * half) % MOD;
+        if (exp % 2 == 1) {
+            res = (res * base) % MOD;
         }
-        return res % m;
+        return res;
     }
 
-public:
     int countGoodNumbers(long long n) {
-        long long a = n / 2;
-        long long evenCount = (n % 2 == 1) ? binpow(5 , a + 1) : binpow(5 , a);
-        long long oddCount = binpow(4 , a);
-
-        long long ans = evenCount * oddCount;
-        return ans % m;
+        long long evenCount = modPow(5, (n + 1) / 2);
+        long long oddCount  = modPow(4, n / 2);
+        return (evenCount * oddCount) % MOD;
     }
 };
