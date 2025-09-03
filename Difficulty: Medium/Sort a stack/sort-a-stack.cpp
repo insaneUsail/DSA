21e022321dd9cadca115e952aecd1f2d32c93b4f
Usail khan <86511043+insaneUsail@users.noreply.github.com>
2025-09-03 05:54:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -8,14 +8,30 @@ public:
 
 /* The below method sorts the stack s
 you are required to complete the below method */
+
+void dfs(int x,stack<int>& s){
+   
+    
+    if(s.empty() || x>s.top()){
+        s.push(x);
+
+    }else{
+        int y=s.top();
+        s.pop();
+        dfs(x,s);
+        s.push(y);
+    }
+}
+void reverse(stack<int>& s){
+    if(!s.empty()){
+    int x=s.top();
+    s.pop();
+    reverse(s);
+    dfs(x,s);
+        
+    }
+}
+
 void SortedStack ::sort() {
- vector<int> v;
-      while(!s.empty()){
-         v.push_back(s.top());
-         s.pop();
-      }
-      std::sort(v.begin(),v.end());
-      for(int i = 0;i<v.size();i++){
-          s.push(v[i]);
-      }
+reverse(s);
 }
