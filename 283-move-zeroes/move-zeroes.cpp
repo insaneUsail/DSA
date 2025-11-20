61077e87bef43691bed20a53e49abb4c4ecb55ee
Usail khan <86511043+insaneUsail@users.noreply.github.com>
2025-11-20 15:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (20.23%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i=0;
-        if(nums.size()<=1){
-            nums;
-        }
-        for(int j=1;j<nums.size();){
+        int n = nums.size();
+        if(n <= 1) return;
 
+        int i = 0;
+        int j = 1;
 
-            if(nums[i]==0 && nums[j]!=0){
-                nums[i]=nums[j];
-                nums[j]=0;
+        while(j < n){
+
+            if(nums[i] == 0 && nums[j] != 0){
+                nums[i] = nums[j];
+                nums[j] = 0;
                 i++;
                 j++;
             }
-           else if(nums[i]==0 && nums[j]==0){
-           j++;
+            else if(nums[i] == 0 && nums[j] == 0){
+                j++;
             }
-            else if(nums[i]!=0 && nums[j]==0){
+            else if(nums[i] != 0 && nums[j] == 0){
+                i++;
+                if(i == j) j++;   
+            }
+            else if(nums[i] != 0 && nums[j] != 0){
                 i++;
                 j++;
             }
-            else if(nums[i]!=0 && nums[j]!=0){
-                i++;
-                j++;
-            }
+
+            if(i >= j) j++;   
         }
     }
 };
