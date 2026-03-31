c0e3498053a37e78b6441f0426d510bcc2e887bd
Usail khan <86511043+insaneUsail@users.noreply.github.com>
2026-03-31 18:30:16 +0530

    Time: 415 ms (13.23%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,4 +1,13 @@
-# Write your MySQL query statement below
-select q.query_name,round(avg(q.rating/q.position),2) as quality,round(sum(rating<'3')/count(q.query_name)*100,2) as poor_query_percentage
-from Queries q
-group by q.query_name
+-- # Write your MySQL query statement below
+-- select q.query_name,round(avg(q.rating/q.position),2) as quality,round(sum(rating<'3')/count(q.query_name)*100,2) as poor_query_percentage
+-- from Queries q
+-- group by q.query_name
+
+
+
+
+
+
+select query_name, round(avg(p.rating/p.position),2) as quality, round(avg(p.rating<3)*100,2) as poor_query_percentage
+from Queries p
+group by query_name
